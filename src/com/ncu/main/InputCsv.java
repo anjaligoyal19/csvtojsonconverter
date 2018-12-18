cca383531d9af9bd87ff0d1a2cd437571831d357
anjaligoyal19 <shakshisharma@ncuindia.edu>
2018-12-18 11:57:38 +0530

    Added keep file in classes

---
+++ src/com/ncu/main/InputCsv.java
@@ -0,0 +1,17 @@
+package com.ncu.main
+
+class InputCsv
+{
+   
+ public static void main(String args[])
+Scanner scanner = new Scanner(System. in);
+	
+logger.out.println("Please enter valid destination json file name");
+
+ String input = scanner. nextLine();
+
+//if (StringUtils.isBlank(String input))
+//logger.out.println("error")
+//else 
+//continue;	
+}
